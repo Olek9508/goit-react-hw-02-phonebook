f841ad7c7cf10f0fae5704b8854586e41d1545c9
Oleksandr Volik <polskasasha@gmail.com>
2022-09-15 02:04:35 +0300

    add some code

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,16 +1,23 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
+import { Component } from "react";
+import { ContactForm } from "components/ContactForm/ContactForm";
+import { Filter } from "components/Filter/Filter";
+// import {ContactList} from "components/ContactList/ContactList"
+
+export class App extends Component  {
+  state = {
+  contacts: [],
+  name: '',
+  }
+  render() {
+    return (
+<div>
+  <h1>Phonebook</h1>
+  <ContactForm/>
+
+  <h2>Contacts</h2>
+    <Filter />
+    {/* <ContactList/> */}
+  </div>
+    )
+  }
 };
---
+++ src/components/ContactForm/ContactForm.jsx
@@ -0,0 +1,47 @@
+import { Component } from "react";
+
+export class ContactForm extends Component{
+  state = {
+  contacts: [],
+  name: '',
+  number: '',
+  }
+
+  onSearchByInputValue = event => {
+    const {name, value} = event.currentTarget
+    this.setState({ [name]:value })
+  }
+  
+  render() {
+    return (
+    <div>
+      <form>
+        <label>
+          Name
+        <input
+          type="text"
+          name="name"
+          value={this.state.name}
+          onChange = {this.onSearchByInputValue}
+          pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
+          title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
+          required
+          />
+        </label>
+        <label >
+          Number
+        <input
+          type="tel"
+          name="number"
+          value={this.state.number}
+          onChange = {this.onSearchByInputValue}
+          pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
+          title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
+          required
+        />
+          </label>
+      </form>
+    </div>
+    )
+  }
+}
